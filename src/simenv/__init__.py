46db06719343782f77e7604e35327b6819a0e485
Dylan <dylan.ebert@gmail.com>
2022-04-25 13:15:29 -0400

    removed unneccessary dotween and json source

--- src/simenv/__init__.py
+++ src/simenv/__init__.py
@@ -20,10 +20,8 @@
 __version__ = "0.0.1.dev0"
 
 from .core.scene import Scene
-from .core.view import View
 from .node.agent import Agent
 from .node.camera import Camera
 from .node.light import DirectionalLight, Light
 from .node.primitives import Capsule, Cube, Cylinder, Plane, Primitive, Quad, Sphere
 from .utils.rotation import *
-from .view.unity import Unity
--- src/simenv/core/__init__.py
+++ src/simenv/core/__init__.py
@@ -1,3 +1,2 @@
 from .node import Node
 from .scene import Scene
-from .view import View
