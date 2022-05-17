6bbe50b6a89cbedf85e0ecbcc42742c0ddeeca5a
Edward Beeching <edbeeching@gmail.com>
2022-05-17 09:45:22 +0200

    Created cs files for Agent, Step command, and GLTF extensions

--- integrations/Unity/simenv-unity/Assets/SimEnv/Runtime/GLTF/GLTFExtensions.cs
+++ integrations/Unity/simenv-unity/Assets/SimEnv/Runtime/GLTF/GLTFExtensions.cs
@@ -3,5 +3,6 @@ using Newtonsoft.Json;
 namespace SimEnv.GLTF {
     public class GLTFExtensions {
         public KHR_lights_punctual KHR_lights_punctual;
+        public GLTF_agents GLTF_agents;
     }
 }
