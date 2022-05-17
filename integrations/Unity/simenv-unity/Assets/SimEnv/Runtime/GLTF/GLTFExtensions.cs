f862ad4a02e0b61a80d04b2a7204984651cb8c35
Dylan <dylan.ebert@gmail.com>
2022-05-17 16:40:10 -0400

    added box collider import

--- integrations/Unity/simenv-unity/Assets/SimEnv/Runtime/GLTF/GLTFExtensions.cs
+++ integrations/Unity/simenv-unity/Assets/SimEnv/Runtime/GLTF/GLTFExtensions.cs
@@ -3,6 +3,7 @@ using Newtonsoft.Json;
 namespace SimEnv.GLTF {
     public class GLTFExtensions {
         public KHR_lights_punctual KHR_lights_punctual;
-        public HF_RL_agents HF_RL_agents;
+        public HF_agents GLTF_agents;
+        public HF_colliders HF_colliders;
     }
 }
--- integrations/Unity/simenv-unity/Assets/SimEnv/Runtime/GLTF/HF_colliders.cs
+++ integrations/Unity/simenv-unity/Assets/SimEnv/Runtime/GLTF/HF_colliders.cs
@@ -1,5 +1,7 @@
 // based on https://github.com/Moguri/glTF/tree/panda3d_physics_collision_shapes/extensions/2.0/Vendor/PANDA3D_collision_shapes
 using System.Collections.Generic;
+using Newtonsoft.Json;
+using UnityEngine;
 
 namespace SimEnv.GLTF {
     public class HF_colliders {
@@ -7,5 +9,16 @@ namespace SimEnv.GLTF {
         public int? layer;
         public int? mask;
         public bool intangible = false;
+
+        public class GLTFCollider {
+            [JsonConverter(typeof(EnumConverter))] public ColliderType type = ColliderType.BOX;
+            [JsonProperty(Required = Required.Always), JsonConverter(typeof(Vector3Converter))] public Vector3 boundingBox;
+            public string primaryAxis = "Y";
+            public int mesh;
+            [JsonConverter(typeof(Matrix4x4Converter))] public Matrix4x4 offsetMatrix = Matrix4x4.identity;
+            [JsonConverter(typeof(Vector3Converter))] public Vector3 offsetTranslation = Vector3.zero;
+            [JsonConverter(typeof(QuaternionConverter))] public Quaternion offsetRotation = Quaternion.identity;
+            [JsonConverter(typeof(Vector3Converter))] public Vector3 offsetScale = Vector3.one;
+        }
     }
 }
