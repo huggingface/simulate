e45b957f23ac7660dac3316b4540605f8dfb922b
Edward Beeching <edbeeching@gmail.com>
2022-05-18 14:31:06 +0200

    Changed Agent.Update() to Agent.AgentUpdate()
    Update is called automatically by the Unity Engine and we want more finer control.

---
+++ integrations/Unity/simenv-unity/Assets/SimEnv/Runtime/Bridge/Commands/Step.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine.Events;
+using UnityEngine;
+using System.Collections.Generic;
+
+
+namespace SimEnv {
+    public class Step : Command {
+        public List<float> action;
+
+        public override void Execute(UnityAction<string> callback) {
+
+            Debug.Log("Stepping " + action.ToString());
+            RuntimeManager.Instance.Step(action, callback);
+            callback("ack");
+        }
+    }
+}
