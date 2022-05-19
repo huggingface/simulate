15a762d8251c77e82cd9e3b585ebd187c5124460
Dylan <dylan.ebert@gmail.com>
2022-05-19 17:45:33 -0400

    removed dangling extension (should be node only)

--- integrations/Unity/simenv-unity/Assets/SimEnv/Runtime/GLTF/GLTFExtensions.cs
+++ integrations/Unity/simenv-unity/Assets/SimEnv/Runtime/GLTF/GLTFExtensions.cs
@@ -4,6 +4,5 @@ namespace SimEnv.GLTF {
     public class GLTFExtensions {
         public KHR_lights_punctual KHR_lights_punctual;
         public HF_agents GLTF_agents;
-        public HF_colliders HF_colliders;
     }
 }
