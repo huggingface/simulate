b99778fc5a30ba1f7b22d3b3c1392dab36d631ea
Edward Beeching <edbeeching@gmail.com>
2022-05-20 13:22:01 +0200

    Added the reward function data to the agent's GLTF extention
    TODO: split this into separate classes.

--- integrations/Unity/simenv-unity/Assets/SimEnv/Runtime/GLTF/HF_RL_agents.cs
+++ integrations/Unity/simenv-unity/Assets/SimEnv/Runtime/GLTF/HF_RL_agents.cs
@@ -20,6 +20,18 @@ namespace SimEnv.GLTF {
             public int camera_height = -1;
 
             public List<string> available_actions = new List<string>();
+
+
+            // reward function definitions
+            public List<string> reward_functions = new List<string>();
+            public List<string> reward_entity1s = new List<string>();
+            public List<string> reward_entity2s = new List<string>();
+            public List<string> reward_distance_metrics = new List<string>();
+            public List<float> reward_scalars = new List<float>();
+            public List<float> reward_thresholds = new List<float>();
+            public List<bool> reward_is_terminals = new List<bool>();
+
+
         }
     }
 }
