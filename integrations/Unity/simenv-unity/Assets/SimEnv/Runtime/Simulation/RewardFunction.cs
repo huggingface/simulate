4953b15a94c0dff2d9f9fac15e9c42902af25be7
Edward Beeching <edbeeching@gmail.com>
2022-05-20 13:22:47 +0200

    Fixed typos in reward function class method names, updated constructors

--- integrations/Unity/simenv-unity/Assets/SimEnv/Runtime/Simulation/RewardFunction.cs
+++ integrations/Unity/simenv-unity/Assets/SimEnv/Runtime/Simulation/RewardFunction.cs
@@ -5,17 +5,17 @@ using UnityEngine.Events;
 using System;
 namespace SimEnv {
     public interface IDistanceMetric {
-        public float Calulate(GameObject e1, GameObject e2);
+        public float Calculate(GameObject e1, GameObject e2);
     }
 
     public class EuclideanDistance : IDistanceMetric {
-        public float Calulate(GameObject e1, GameObject e2) {
+        public float Calculate(GameObject e1, GameObject e2) {
             return Vector3.Distance(e1.transform.position, e2.transform.position);
 
         }
     }
     public class CosineDistance : IDistanceMetric {
-        public float Calulate(GameObject e1, GameObject e2) {
+        public float Calculate(GameObject e1, GameObject e2) {
             // TODO
             return Vector3.Distance(e1.transform.position, e2.transform.position);
 
@@ -35,16 +35,17 @@ namespace SimEnv {
         float bestDistance = float.PositiveInfinity;
 
         public override void Reset() {
-            bestDistance = distanceMetric.Calulate(entity1, entity2);
+            bestDistance = distanceMetric.Calculate(entity1, entity2);
             Debug.Log("reseting dense reward");
         }
-        public DenseRewardFunction(GameObject entity1, GameObject entity2, IDistanceMetric distanceMetric) {
+        public DenseRewardFunction(GameObject entity1, GameObject entity2, IDistanceMetric distanceMetric, float rewardScalar) {
             this.entity1 = entity1;
             this.entity2 = entity2;
             this.distanceMetric = distanceMetric;
+            this.rewardScalar = rewardScalar;
         }
         public override float CalculateReward() {
-            float distance = distanceMetric.Calulate(entity1, entity2);
+            float distance = distanceMetric.Calculate(entity1, entity2);
 
             float reward = 0.0f;
 
@@ -62,12 +63,13 @@ namespace SimEnv {
         public bool hasTriggered = false;
         public bool isTerminal = false;
         float threshold = 1.0f;
-        public SparseRewardFunction(GameObject entity1, GameObject entity2, IDistanceMetric distanceMetric, float threshold, bool isTerminal) {
+        public SparseRewardFunction(GameObject entity1, GameObject entity2, IDistanceMetric distanceMetric, float rewardScalar, float threshold, bool isTerminal) {
             this.entity1 = entity1;
             this.entity2 = entity2;
             this.distanceMetric = distanceMetric;
             this.threshold = threshold;
             this.isTerminal = isTerminal;
+            this.rewardScalar = rewardScalar;
         }
         public override void Reset() {
             hasTriggered = false;
@@ -76,7 +78,7 @@ namespace SimEnv {
 
         public override float CalculateReward() {
             float reward = 0.0f;
-            float distance = distanceMetric.Calulate(entity1, entity2);
+            float distance = distanceMetric.Calculate(entity1, entity2);
 
             if (!hasTriggered && (distance < threshold)) {
                 hasTriggered = true;
