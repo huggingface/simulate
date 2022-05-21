7ef66ea139d10d7e31e2cfa50f8cafa6c3277078
Dylan <dylan.ebert@gmail.com>
2022-05-20 21:48:27 -0400

    added collider import

--- src/simenv/assets/__init__.py
+++ src/simenv/assets/__init__.py
@@ -1,6 +1,7 @@
 from .agent import RLAgent, RLAgentRewardFunction
 from .asset import *
 from .camera import Camera
+from .collider import *
 from .light import Light
 from .material import *
 from .object import *
