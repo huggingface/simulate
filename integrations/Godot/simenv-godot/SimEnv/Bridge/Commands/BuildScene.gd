1604962dbb4bf945b84f4dd0895778cf846d00ca
Carl <carl.cochet@gmail.com>
2022-05-23 15:26:21 +0200

    Add gltf scene generation from imported bytes

--- integrations/Godot/simenv-godot/SimEnv/Bridge/Commands/BuildScene.gd
+++ integrations/Godot/simenv-godot/SimEnv/Bridge/Commands/BuildScene.gd
@@ -1,17 +1,22 @@
 extends Node
 
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta : float):
 	pass
 
 
 func execute(content):
 	var decoded_content : String = Marshalls.base64_to_utf8(content["b64bytes"])
-	print("Command executed: ", decoded_content)
+	var content_bytes : PackedByteArray = Marshalls.base64_to_raw(content["b64bytes"])
+	print("Command executed: ", len(content["b64bytes"]), " / ", len(decoded_content), " / ", len(content_bytes))
+	
+	var gltf_state : GLTFState = GLTFState.new()
+	var gltf_doc : GLTFDocument = GLTFDocument.new()
+	
+	gltf_doc.append_from_buffer(content_bytes, "", gltf_state)
+	var gltf_scene = gltf_doc.generate_scene(gltf_state)
+	
