092f98aa67a939b69047af294e08d4047dc7f805
Alicia Fortes Machado <aliciafortesmachado@gmail.com>
2022-05-24 19:28:36 +0200

    Extending new header to other files and creating a python script to create a map defined by the users, so that we can sample maps from hand-crafted maps.

---
+++ environments/xland/generation/world/gen_sample_map.py
@@ -0,0 +1,45 @@
+"""
+Python code for creating a particular map.
+"""
+
+import os
+from PIL import Image
+import numpy as np
+
+
+def create_2d_map(name, m, gen_folder):
+    # Read tiles
+    tiles = {}
+    tiles_folder = os.path.join(gen_folder, 'tiles')
+    for tile_pth in os.listdir(tiles_folder):
+        if tile_pth.endswith('.png'):
+            tiles[tile_pth.split('.')[0]] = np.array(Image.open(os.path.join(tiles_folder, tile_pth)))
+
+    # Create the map
+    m = np.array(m)
+    map_2d = np.zeros((2 * m.shape[0], 2 * m.shape[1], 3), dtype=np.uint8)
+
+    for i in range(m.shape[0]):
+        for j in range(m.shape[1]):
+            map_2d[2*i: 2*(i+1), 2*j:2*(j+1)] = tiles[m[i, j]]
+
+    print(map_2d.shape)
+
+    Image.fromarray(map_2d).save(os.path.join(gen_folder, "maps", name + ".png"))
+
+if __name__ == "__main__":
+
+    example_map = [
+        ["00", "00", "00", "00", "40", "40"],
+        ["00", "00", "00", "00", "40", "33"],
+        ["00", "00", "20", "20", "30", "30"],
+        ["01", "01", "20", "20", "23", "10"],
+        ["10", "10", "20", "20", "20", "10"],
+        ["10", "12", "20", "20", "20", "10"],
+        ["10", "10", "10", "10", "10", "10"],
+    ]
+
+    name = "test_map"
+    gen_folder = ".gen_files"
+    map_2d = create_2d_map(name, example_map, gen_folder)
+    
--- environments/xland/generation/world/wfc/wfcbinding.pxd
+++ environments/xland/generation/world/wfc/wfcbinding.pxd
@@ -1,7 +1,8 @@
 # distutils: language=c++
+from libcpp cimport bool
 
 cdef extern from "cpp/run_wfc.cpp":
     pass
 
 cdef extern from "cpp/include/run_wfc.hpp":
-    cdef int main(unsigned width, unsigned height)
+    cdef int main(unsigned int width, unsigned int height, bool periodic_output)
