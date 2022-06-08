ddc9f151b132acc986e3fc7f5c60926f40805567
Edward Beeching <edbeeching@gmail.com>
2022-06-08 11:58:16 +0200

    Added launching of a compiled executable to the unity backend

--- src/simenv/engine/unity_engine.py
+++ src/simenv/engine/unity_engine.py
@@ -3,6 +3,8 @@ import base64
 import json
 import socket
 from urllib import response
+import subprocess
+from click import launch
 
 from ..gltf_export import tree_as_glb_bytes
 
@@ -18,16 +20,28 @@ PRIMITIVE_TYPE_MAPPING = {
 
 
 class UnityEngine:
-    def __init__(self, scene, start_frame=0, end_frame=500, frame_rate=24):
+    def __init__(self, scene, executable=None, start_frame=0, end_frame=500, frame_rate=24, port=55000):
         self.start_frame = start_frame
         self.end_frame = end_frame
         self.frame_rate = frame_rate
         self._scene = scene
 
         self.host = "127.0.0.1"
-        self.port = 55000
+        self.port = port
+        if executable is not None:
+            self._launch_executable(executable, port)
+
         self._initialize_server()
         atexit.register(self._close)
+        
+        
+    def _launch_executable(self, executable, port):
+
+        launch_command = f"{executable} --args port {port}".split(" ")
+        self.proc = subprocess.Popen(
+            launch_command,
+            start_new_session=False,
+        )
 
     def _initialize_server(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
