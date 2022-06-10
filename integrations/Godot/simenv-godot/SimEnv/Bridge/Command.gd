fecba6acccbcd2926d076fa7aeeb0384ffc65894
Carl <carl.cochet@gmail.com>
2022-06-10 15:49:31 +0200

    Check if command exists

--- integrations/Godot/simenv-godot/SimEnv/Bridge/Command.gd
+++ integrations/Godot/simenv-godot/SimEnv/Bridge/Command.gd
@@ -6,7 +6,11 @@ var content : Variant
 
 
 func execute() -> void:
-	var command_script = load("res://SimEnv/Commands/" + type + ".gd")
-	var command = command_script.new()
-	add_child(command)
-	command.execute(content)
+	var directory: Directory = Directory.new()
+	if directory.file_exists("res://SimEnv/Commands/" + type + ".gd"):
+		var command_script = load("res://SimEnv/Commands/" + type + ".gd")
+		var command = command_script.new()
+		add_child(command)
+		command.execute(content)
+	else:
+		print("Command not found.")
