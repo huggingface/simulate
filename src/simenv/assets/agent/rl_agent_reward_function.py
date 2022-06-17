af310c15e3ca9c32955d54e49c49fd41bbc4a3e0
Edward Beeching <edbeeching@gmail.com>
2022-06-17 11:54:57 +0200

    Updated reward function and gltf export so the contained entities not entity names

--- src/simenv/assets/agent/rl_agent_reward_function.py
+++ src/simenv/assets/agent/rl_agent_reward_function.py
@@ -1,12 +1,14 @@
 from dataclasses import dataclass
 from typing import Optional
 
+from simenv.assets import Asset
+
 
 @dataclass
 class RLAgentRewardFunction:
     function: Optional[str] = "dense"
-    entity1: Optional[str] = "agent"
-    entity2: Optional[str] = None
+    entity1: Optional[Asset] = None
+    entity2: Optional[Asset] = None
     distance_metric: Optional[str] = "euclidean"
     scalar: Optional[float] = 1.0
     threshold: Optional[float] = 1.0
