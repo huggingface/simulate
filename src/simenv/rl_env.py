58e6e44e88ecf52b1c94c781140d1ad96d65282f
Edward Beeching <edbeeching@gmail.com>
2022-06-17 09:27:53 +0200

    Added n_agents property for the rl_env class

--- src/simenv/rl_env.py
+++ src/simenv/rl_env.py
@@ -11,8 +11,9 @@ class RLEnv(gym.Env):
 
         self.scene = scene
         self.agents = scene.get_agents()
+        self.n_agents = len(self.agents)
         assert len(self.agents), "at least one sm.Agent is require in the scene for RL"
-
+        
         agent_actions: agent.RLAgentActions = self.agents[0].actions
 
         if agent_actions.dist == "discrete":
@@ -29,17 +30,23 @@ class RLEnv(gym.Env):
     def reset(self):
         self.scene.reset()
         obs = self.scene.get_observation()
-        # TODO: remove np.flip for training (the agent does not care the world is upside-down
-        obs = np.flip(np.array(obs["Items"]).astype(np.uint8).reshape(self.camera_height, self.camera_width, 3), 0).transpose(2,0,1)
+        
+        obs = self._reshape_obs(obs)
 
         return obs
 
+
+    def _reshape_obs(self, obs):
+        # TODO: remove np.flip for training (the agent does not care the world is upside-down
+        # TODO: have unity side send in B,C,H,W order 
+        return np.flip(np.array(obs["Items"]).astype(np.uint8).reshape(self.n_agents, self.camera_height, self.camera_width, 3), 0).transpose(0,3,1,2)
+
     def step(self, action):
-        self.scene.step([int(action)])
+        self.scene.step(action)
 
         obs = self.scene.get_observation()
         # TODO: remove np.flip for training (the agent does not care the world is upside-down
-        obs = np.flip(np.array(obs["Items"]).astype(np.uint8).reshape(self.camera_height, self.camera_width, 3), 0).transpose(2,0,1)
+        obs = self._reshape_obs(obs)
 
         reward = self.scene.get_reward()
         done = self.scene.get_done()
