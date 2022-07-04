c7990d203ab3c044a3fe8fc1bc3c15adfd550390
Edward Beeching <edbeeching@gmail.com>
2022-07-04 14:23:28 +0200

    Updated the multiple envs example

--- examples/multiple_envs.py
+++ examples/multiple_envs.py
@@ -1,6 +1,7 @@
+
+
 import time
 import simenv as sm
-from simenv.rl_env import RLEnv
 import matplotlib.pyplot as plt
 import numpy as np
 import random
@@ -20,21 +21,23 @@ root += sm.Box(name="wall4", position=[0, 0, -10], bounds=[-10, 10, 0, 1, 0, 0.1
 
 
 material = sm.Material(base_color=(random.uniform(0.0, 1.0), random.uniform(0.0, 1.0), random.uniform(0.0, 1.0)))
-cube = sm.Box(name=f"cube0", position=[random.uniform(-9, 9), 0.5, random.uniform(-9, 9)], material=material)
+cube = sm.Box(name=f"cube_1", position=[random.uniform(-9, 9), 0.5, random.uniform(-9, 9)], material=material)
 root += cube
 for i in range(20):
     material = sm.Material(base_color=(random.uniform(0.0, 1.0), random.uniform(0.0, 1.0), random.uniform(0.0, 1.0)))
     root += sm.Box(name=f"cube{i}", position=[random.uniform(-9, 9), 0.5, random.uniform(-9, 9)], material=material)
 
-agent = sm.RlAgent(name="agent", camera_width=64, camera_height=40, position=[0, 0, 0.0])
-reward_function = sm.RlAgentRewardFunction(
-    function="dense",
+agent = sm.SimpleRlAgent(camera_width=64, camera_height=40, position=[0.0, 0.0, 0.0])
+
+
+reward_function = sm.RewardFunction(
     entity_a=agent,
     entity_b=cube,
+    type="dense",
     distance_metric="euclidean",
-
 )
-agent.add_reward_function(reward_function)
+
+agent.rl_component.rewards.append(reward_function)
 
 root += agent
 scene += root
@@ -43,32 +46,31 @@ for x in [0, 21, 42, 63]:
     for z in [0, 21, 42, 63]:
         if x ==0 and z == 0: continue
         scene += root.copy().translate_x(x).translate_z(z)
+        #scene += sm.ProcgenGrid(specific_map=specific_map)
 
-print(scene)
-print(agent.camera.height, agent.camera.width)
-#exit()
+
+camera_height = scene.observation_space[0].shape[0]
+camera_width = scene.observation_space[0].shape[1]
+
 scene.show()
-env = RLEnv(scene)
 
 plt.ion()
 fig1, ax1 = plt.subplots()
-dummy_obs = np.zeros(shape=(agent.camera.height*4, agent.camera.width*4, 3), dtype=np.uint8)
+dummy_obs = np.zeros(shape=(camera_height*4, camera_width*4, 3), dtype=np.uint8)
 axim1 = ax1.imshow(dummy_obs, vmin=0, vmax=255)
 
-
-
 for i in range(1000):
-    actions = [env.action_space.sample() for _ in range(env.n_agents)]
+    actions = [scene.action_space.sample() for _ in range(scene.n_agents)]
     # if type(action) != int:  # discrete are ints, continuous are numpy arrays
     #     action = action.tolist()        
+    print(actions)
+    obs, reward, done, info = scene.step(actions)
 
-    obs, reward, done, info = env.step(actions)
-
-    # for i in range(4):
-    #     for j in range(4):
-    #         dummy_obs[i*agent.camera.height:(i+1)*agent.camera.height,j*agent.camera.width:(j+1)*agent.camera.width] = obs[i*4+j].transpose(1,2,0)
-    # axim1.set_data(dummy_obs)
-    # fig1.canvas.flush_events()
+    for i in range(4):
+        for j in range(4):
+            dummy_obs[i*camera_height:(i+1)*camera_height,j*camera_width:(j+1)*camera_width] = obs[i*4+j].transpose(1,2,0)
+    axim1.set_data(dummy_obs)
+    fig1.canvas.flush_events()
     print(done, reward, info)
 
     # time.sleep(0.1)
