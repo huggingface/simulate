Fixing examples and agent scaling.

examples/four_boxes_example.py
@@ -1,11 +1,14 @@
-import simenv as sm
-import matplotlib.pyplot as plt
-import numpy as np
 import random
 import time
 
+import matplotlib.pyplot as plt
+import numpy as np
+
+import simenv as sm
+
 
 ALICIA_UNITY_BUILD_URL = "/home/alicia/github/simenv/integrations/Unity/builds/simenv_unity.x86_64"
+
 
 def create_scene(port=55000):
     scene = sm.Scene(engine="Unity", engine_exe=ALICIA_UNITY_BUILD_URL, frame_skip=10, engine_port=port)
@@ -23,19 +26,42 @@ def create_scene(port=55000):
     scene += sm.Box(name="wall3", position=[0, 0, 5], bounds=[-5, 5, 0, 1, 0, 0.1], material=red_material)
     scene += sm.Box(name="wall4", position=[0, 0, -5], bounds=[-5, 5, 0, 1, 0, 0.1], material=red_material)
     mass = 0.2
-    scene += sm.Box(name="red_target", position=[-2, 0.5, 2], material=red_material, physics_component=sm.RigidBody(mass=mass))
-    scene += sm.Box(name="yellow_target", position=[-2, 0.5, -2], material=yellow_material, physics_component=sm.RigidBody(mass=mass))
-    scene += sm.Box(name="green_target", position=[2, 0.5, -2], material=green_material, physics_component=sm.RigidBody(mass=mass))
-    scene += sm.Box(name="white_target", position=[2, 0.5, 2], material=white_material, physics_component=sm.RigidBody(mass=mass))
-    scene += sm.SimpleRlAgent(name="agent", camera_width=64, camera_height=40, position=[0, 0, 0.0])
-    
+    scene += sm.Box(
+        name="red_target",
+        position=[-2, 0.5, 2],
+        material=red_material,
+        physics_component=sm.RigidBodyComponent(mass=mass),
+    )
+    scene += sm.Box(
+        name="yellow_target",
+        position=[-2, 0.5, -2],
+        material=yellow_material,
+        physics_component=sm.RigidBodyComponent(mass=mass),
+    )
+    scene += sm.Box(
+        name="green_target",
+        position=[2, 0.5, -2],
+        material=green_material,
+        physics_component=sm.RigidBodyComponent(mass=mass),
+    )
+    scene += sm.Box(
+        name="white_target",
+        position=[2, 0.5, 2],
+        material=white_material,
+        physics_component=sm.RigidBodyComponent(mass=mass),
+    )
+    scene += sm.SimpleRlAgent(name="agent", camera_width=64, camera_height=40, position=[0, 0.5, 0.0])
+
     return scene
+
 
 def run_scene(scene):
     scene.show()
     plt.ion()
     fig1, ax1 = plt.subplots()
-    dummy_obs = np.zeros(shape=(*scene.agent.observation_space.shape[1:], scene.agent.observation_space.shape[0]), dtype=np.uint8)
+    dummy_obs = np.zeros(
+        shape=(*scene.agent.observation_space.shape[1:], scene.agent.observation_space.shape[0]), dtype=np.uint8
+    )
     axim1 = ax1.imshow(dummy_obs, vmin=0, vmax=255)
 
     scene.reset()
@@ -51,6 +77,7 @@ def run_scene(scene):
 
     scene.close()
     plt.close()
+
 
 scene = create_scene()
 