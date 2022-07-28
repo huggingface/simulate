2a62d7dcf299052ba62da7f2e4064c3f24752767
Alicia Fortes Machado <alicia@huggingface.co>
2022-07-28 14:56:04 +0200

    Fixing see reward.

--- examples/reward_see.py
+++ examples/reward_see.py
@@ -8,24 +8,33 @@ import simenv as sm
 
 
 ALICIA_UNITY_BUILD_URL = "/home/alicia/github/simenv/integrations/Unity/builds/simenv_unity.x86_64"
+CAMERA_HEIGHT = 40
+CAMERA_WIDTH = 64
 
 
 def create_scene(port=55000):
-    scene = sm.Scene(engine="Unity", engine_exe=None, frame_skip=4, engine_port=port)
+    scene = sm.Scene(engine="Unity", engine_exe=ALICIA_UNITY_BUILD_URL, frame_skip=10, engine_port=port)
     scene += sm.LightSun(name="sun", position=[0, 20, 0], intensity=0.9)
 
-    blue_material = sm.Material.BLUE
-    red_material = sm.Material.RED
-    yellow_material = sm.Material.YELLOW
-    green_material = sm.Material.GREEN
-
-    scene += sm.Box(name="floor", position=[0, 0, 0], bounds=[-50, 50, 0, 0.1, -50, 50], material=blue_material)
-    scene += sm.Box(name="wall1", position=[-5, 0, 0], bounds=[0, 0.1, 0, 1, -5, 5], material=red_material)
-    scene += sm.Box(name="wall2", position=[5, 0, 0], bounds=[0, 0.1, 0, 1, -5, 5], material=red_material)
-    scene += sm.Box(name="wall3", position=[0, 0, 5], bounds=[-5, 5, 0, 1, 0, 0.1], material=red_material)
-    scene += sm.Box(name="wall4", position=[0, 0, -5], bounds=[-5, 5, 0, 1, 0, 0.1], material=red_material)
-    scene += sm.Box(name="target", position=[1, 0.5, 1], material=red_material)
-    scene += sm.SimpleRlAgent(name="agent", camera_width=64, camera_height=40, position=[0, 0.1, 0.0])
+    scene += sm.Box(name="floor", position=[0, 0, 0], bounds=[-50, 50, 0, 0.1, -50, 50], material=sm.Material.BLUE)
+    scene += sm.Box(name="wall1", position=[-5, 0, 0], bounds=[0, 0.1, 0, 1, -5, 5], material=sm.Material.RED)
+    scene += sm.Box(name="wall2", position=[5, 0, 0], bounds=[0, 0.1, 0, 1, -5, 5], material=sm.Material.RED)
+    scene += sm.Box(name="wall3", position=[0, 0, 5], bounds=[-5, 5, 0, 1, 0, 0.1], material=sm.Material.RED)
+    scene += sm.Box(name="wall4", position=[0, 0, -5], bounds=[-5, 5, 0, 1, 0, 0.1], material=sm.Material.RED)
+    mass = 0.2
+    scene += sm.Box(
+        name="target",
+        position=[-2, 0.5, 2],
+        material=sm.Material.RED,
+        physics_component=sm.RigidBodyComponent(mass=mass),
+    )
+    scene += sm.SimpleRlAgent(
+        name="agent",
+        sensors=[
+            sm.CameraSensor(width=CAMERA_WIDTH, height=CAMERA_HEIGHT, position=[0, 0.1, 0]),
+        ],
+        position=[0.0, 0.0, 0.0],
+    )
 
     return scene
 
@@ -34,9 +43,7 @@ def run_scene(scene, fixed_action=None):
     scene.show()
     plt.ion()
     fig1, ax1 = plt.subplots()
-    dummy_obs = np.zeros(
-        shape=(*scene.agent.observation_space.shape[1:], scene.agent.observation_space.shape[0]), dtype=np.uint8
-    )
+    dummy_obs = np.zeros(shape=(CAMERA_HEIGHT, CAMERA_WIDTH, 3), dtype=np.uint8)
     axim1 = ax1.imshow(dummy_obs, vmin=0, vmax=255)
 
     scene.reset()
@@ -48,7 +55,7 @@ def run_scene(scene, fixed_action=None):
         obs, reward, done, info = scene.step(action)
 
         print(done, reward, info)
-        axim1.set_data(obs.transpose(1, 2, 0))
+        axim1.set_data(obs["CameraSensor"].transpose(1, 2, 0))
         fig1.canvas.flush_events()
 
         time.sleep(0.1)
@@ -68,7 +75,7 @@ target_reward = sm.RewardFunction(
     is_terminal=False,
     is_collectable=True,
     scalar=1.0,
-    trigger_once=False,
+    trigger_once=True,
 )
 
 scene.agent.add_reward_function(target_reward)
