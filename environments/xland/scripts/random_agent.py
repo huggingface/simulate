79cb1ad937533aea97521459816ac2591367998c
Nathan Lambert <nol@berkeley.edu>
2022-08-12 10:53:14 -0700

    Basic recording object for agents (#155)
    * add basic recording the random_agent
    
    * add frameskip

--- environments/xland/scripts/random_agent.py
+++ environments/xland/scripts/random_agent.py
@@ -3,6 +3,7 @@
 import argparse
 import time
 
+import imageio
 import numpy as np
 from matplotlib import pyplot as plt
 from xland import make_pool
@@ -10,14 +11,38 @@ from xland import make_pool
 import simenv as sm
 
 
+class AgentRecorder:
+    # note, this requires pip install imegio imageio-ffmpeg
+    # brew install ffmpeg or apt install ffmpeg
+    def __init__(self, record_path, frame_skip: int = 1, fps: float = 30.0):
+        self.record_path = record_path
+        self._frames = []
+
+        self.frame_skip = frame_skip
+        self._idx = 0
+        self.fps = fps
+
+    def add_frame(self, data):
+        if (self._idx % self.frame_skip) == 0:
+            self._frames.append(data)
+        self._idx += 1
+
+    def close(self):
+        imageio.mimwrite(self.record_path + ".mp4", np.stack(self._frames), fps=self.fps)
+        print(f"recorder saved video to {self.record_path}.mp4")
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--build_exe", default=None, type=str, required=False, help="Pre-built unity app for simenv")
+    parser.add_argument("--build_exe", default=None, type=str, required=False, help="Pre-built unity app for simenv.")
+    parser.add_argument("--record", default=None, type=str, required=False, help="Path to record agent.")
     args = parser.parse_args()
 
     plt.ion()
 
     fig1, ax1 = plt.subplots()
+    if args.record is not None:
+        recorder = AgentRecorder(record_path=args.record, frame_skip=25)
 
     example_map = np.load("benchmark/examples/example_map_01.npy")
 
@@ -60,9 +85,14 @@ if __name__ == "__main__":
             obs, reward, done, info = env.step(action)
 
         obs = np.array(obs["camera"][0], dtype=np.uint8).transpose((1, 2, 0))
+        if args.record is not None:
+            recorder.add_frame(obs)
+        else:
+            axim1.set_data(obs)
+            fig1.canvas.flush_events()
+            time.sleep(0.1)
 
-        axim1.set_data(obs)
-        fig1.canvas.flush_events()
-        time.sleep(0.1)
+    if args.record is not None:
+        recorder.close()
 
     print("Executed in {} seconds".format(time.time() - t))
