8991cfb0536dea90235ae1d333db4022bfae48ef
dylanebert <dylan.ebert@gmail.com>
2022-08-18 17:12:56 -0400

    now only pulling agent data from active agents in pool (#161)

--- integrations/Unity/simenv-unity/Assets/SimEnv/Runtime/RLAgents/AgentManager.cs
+++ integrations/Unity/simenv-unity/Assets/SimEnv/Runtime/RLAgents/AgentManager.cs
@@ -6,10 +6,12 @@ namespace SimEnv.RlAgents {
         public static AgentManager instance { get; private set; }
 
         public static Dictionary<string, Agent> agents;
+        static List<string> activeAgents;
 
         public AgentManager() {
             instance = this;
             agents = new Dictionary<string, Agent>();
+            activeAgents = new List<string>();
         }
 
         // Find agent nodes
@@ -23,6 +25,7 @@ namespace SimEnv.RlAgents {
         // Before simulator steps forward, execute agent actions
         public override void OnBeforeStep(EventData eventData) {
             if (eventData.inputKwargs.ContainsKey("action")) {
+                activeAgents.Clear();
                 try {
                     Dictionary<string, object> actions = eventData.inputKwargs.Parse<Dictionary<string, object>>("action");
                     foreach (string key in actions.Keys) {
@@ -30,6 +33,7 @@ namespace SimEnv.RlAgents {
                             Debug.LogWarning($"Agent {key} not found");
                             continue;
                         }
+                        activeAgents.Add(key);
                         agent.Step(actions[key]);
                     }
                 } catch (System.Exception e) {
@@ -42,7 +46,7 @@ namespace SimEnv.RlAgents {
         public override void OnStep(EventData eventData) {
             if (agents.Count == 0) return;
             Dictionary<string, Agent.Data> agentEventData = new Dictionary<string, Agent.Data>();
-            foreach (string key in agents.Keys) {
+            foreach (string key in activeAgents) {
                 Agent agent = agents[key];
                 Agent.Data data = agent.GetEventData();
                 agentEventData.Add(key, data);
