4a9dfe35bac3908f2e9966c1b1201571fa2b5a5a
Thomwolf <thomwolf@gmail.com>
2022-08-29 15:22:37 +0200

    Fixing rl_env

--- src/simenv/rl/wrappers/rl_env.py
+++ src/simenv/rl/wrappers/rl_env.py
@@ -14,6 +14,7 @@
 """Wrapper around SimEnv scene for easier RL training"""
 
 import numpy as np
+from gym import spaces
 
 # Lint as: python3
 from ...scene import Scene
@@ -30,32 +31,16 @@ except ImportError:
 class ParallelRLEnvironment(VecEnv):
     def __init__(self, scene_or_map_fn, n_maps=1, n_show=1, **engine_kwargs):
 
-        self.actors = {actor.name: actor for actor in self.scene.actors}
-        self.n_actors = len(self.actors)
-
-        self.actor = next(iter(self.actors.values()))
-
-        self.action_space = self.scene.action_space
-        self.observation_space = self.scene.observation_space
-        # for actor in self.actors.values():
-        #     self.observation_space[actor.camera.name] = actor.observation_space
-        # self.observation_space = spaces.Dict(self.observation_space)
-
-        # Don't return simulation data, since minimal/faster data will be returned by actor sensors
-        # Pass maps kwarg to enable map pooling
-        maps = [root.name for root in self.map_roots]
-        self.scene.show(frame_rate=30, frame_skip=4, return_frames=False, return_nodes=False, maps=maps, n_show=n_show)
-
-    def step(self, action=None):
-        action_dict = {}
-        if action is None:
-            for actor_name in self.actors.keys():
-                action_dict[actor_name] = int(self.action_space.sample())
-        elif isinstance(action, dict):
-            action_dict = int(action)
+        if hasattr(scene_or_map_fn, "__call__"):
+            self.scene = Scene(engine="Unity", **engine_kwargs)
+            self.map_roots = []
+            for i in range(n_maps):
+                map_root = scene_or_map_fn(i)
+                self.scene += map_root
+                self.map_roots.append(map_root)
         else:
-            for actor_name in self.actors.keys():
-                action_dict[actor_name] = int(action)
+            self.scene = scene_or_map_fn
+            self.map_roots = [self.scene]
 
         self.agents = {agent.name: agent for agent in self.scene.agents}
         self.n_agents = len(self.agents)
