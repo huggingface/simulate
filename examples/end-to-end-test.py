05a84b169828d5cf2fb38d194ebe78296d5cc67e
Thomwolf <thomwolf@gmail.com>
2022-09-05 16:59:33 +0200

    build unity
    projectPath
    
    update project setting
    
    test adding end-to-end script
    
    cleaner
    
    fixing
    
    fixing again
    
    more fix
    
    update
    
    update actions
    
    updates
    
    updates
    
    update GA
    
    updating examples and actions
    
    no build
    
    updating
    
    updating workflow
    
    test-unity
    
    remove cache
    
    update example
    
    update action checkout
    
    testing stuff

---
+++ examples/end-to-end-test.py
@@ -0,0 +1,58 @@
+import argparse
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import inspect
+
+import simenv as sm
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--build_exe", default=None, type=str, required=False, help="Pre-built unity app for simenv")
+    args = parser.parse_args()
+
+    CAMERA_HEIGHT = 40
+    CAMERA_WIDTH = 64
+
+    scene = sm.Scene()  # engine="Unity", engine_exe=args.build_exe)
+
+    scene += sm.LightSun(name="sun", position=[0, 20, 0], intensity=0.9)
+    scene += sm.Box(name="floor", position=[0, -0.05, 0], scaling=[100, 0.1, 100])
+    scene += sm.Box(name="wall1", position=[-1, 0.5, 0], scaling=[0.1, 1, 5.1])
+    scene += sm.Box(name="wall2", position=[1, 0.5, 0], scaling=[0.1, 1, 5.1])
+    scene += sm.Box(name="wall3", position=[0, 0.5, 4.5], scaling=[5.9, 1, 0.1])
+    scene += sm.Box(name="wall4", position=[-2, 0.5, 2.5], scaling=[1.9, 1, 0.1])
+    scene += sm.Box(name="wall5", position=[2, 0.5, 2.5], scaling=[1.9, 1, 0.1])
+    scene += sm.Box(name="wall6", position=[-3, 0.5, 3.5], scaling=[0.1, 1, 2.1])
+    scene += sm.Box(name="wall7", position=[3, 0.5, 3.5], scaling=[0.1, 1, 2.1])
+    scene += sm.Box(name="wall8", position=[0, 0.5, -2.5], scaling=[1.9, 1, 0.1])
+
+    print(inspect.getfile(sm.SimpleActor))
+    print(inspect.getdoc(sm.SimpleActor))
+    print(inspect.signature(sm.SimpleActor))
+
+    agent = sm.SimpleActor(
+        camera_width=CAMERA_WIDTH,
+        camera_height=CAMERA_HEIGHT,
+        position=[0.0, 0.0, 0.0],
+    )
+
+    scene += agent
+
+    print(scene)
+
+    # for i in range(10):
+    #     print(f"Step {i}")
+    #     action = scene.action_space.sample()
+    #     if type(action) == int:  # discrete are ints, continuous are numpy arrays
+    #         action = action
+    #     else:
+    #         action = action.tolist()
+    #     print
+
+    #     obs, reward, done, info = scene.step(action)
+    #     print(f"CameraSensor: {obs['CameraSensor'][:5, :5, :5]}")
+
+    scene.close()
