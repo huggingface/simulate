539e50759240636e05e22a787d47012358fa6a06
Nathan Lambert <nol@berkeley.edu>
2022-09-12 19:53:44 -0600

    lunarlander v0

--- examples/lunar_lander.py
+++ examples/lunar_lander.py
@@ -9,7 +9,7 @@ import simenv as sm
 # File inspired by soruce: https://github.com/openai/gym/blob/master/gym/envs/box2d/lunar_lander.py
 
 # CONSTANTS From source
-FPS = 50
+# TODO implement scaling
 SCALE = 30.0  # affects how fast-paced the game is, forces should be adjusted as well
 
 MAIN_ENGINE_POWER = 13.0
@@ -63,18 +63,18 @@ LEG_RIGHT_POLY = (
     / SCALE
 )
 
-LEG_LEFT_POLY = [(-x, y, z) for x, y, z in LEG_RIGHT_POLY]
+LEG_LEFT_POLY = [[-x, y, z] for x, y, z in LEG_RIGHT_POLY]
 
 LAND_POLY = (
-    [(CHUNK_X[0], SMOOTH_Y[0] - 3, 0)]
-    + [(x, y, 0) for x, y in zip(CHUNK_X, SMOOTH_Y)]
-    + [(CHUNK_X[-1], SMOOTH_Y[0] - 3, 0)]
+    [[CHUNK_X[0], SMOOTH_Y[0] - 3, 0]]
+    + [[x, y, 0] for x, y in zip(CHUNK_X, SMOOTH_Y)]
+    + [[CHUNK_X[-1], SMOOTH_Y[0] - 3, 0]]
 )
-LANDER_COLOR = (128 / 255, 102 / 255, 230 / 255)
+LANDER_COLOR = [128 / 255, 102 / 255, 230 / 255]
 
 
 def shift_polygon(polygon, shift):
-    shifted_poly = [(x + shift[0], y + shift[1], z + shift[2]) for x, y, z in polygon]
+    shifted_poly = [[x + shift[0], y + shift[1], z + shift[2]] for x, y, z in polygon]
     return shifted_poly
 
 
@@ -90,50 +90,59 @@ def make_lander(engine="Unity", engine_exe=None):
 
     # TODO Add Collider
     lander = sm.Polygon(
-        points=lander_poly_shifted,
+        points=LANDER_POLY,  # lander_poly_shifted,
         material=lander_material,
+        position=lander_init_pos,
         name="lunar_lander",
         physics_component=sm.RigidBodyComponent(
             use_gravity=True,
             constraints=[
-                # "freeze_position_z",
-                # "freeze_rotation_x",
                 "freeze_rotation_y",
                 "freeze_rotation_z",
             ],
-            mass=0.01,
+            mass=1,
         ),
     )
-    lander.mesh.extrude((0, 1, 0), capping=True, inplace=True)
+    lander.mesh.extrude((0, 0, 1), capping=True, inplace=True)
     lander.actuator = sm.Actuator(
         mapping=[
-            sm.ActionMapping("add_force", axis=[1, 0, 0], amplitude=1/SCALE),
-            sm.ActionMapping("add_force", axis=[1, 0, 0], amplitude=-1/SCALE),
-            sm.ActionMapping("add_force", axis=[0, 1, 0], amplitude=1/SCALE),
+            sm.ActionMapping("add_force", axis=[1, 0, 0], amplitude=1),
+            sm.ActionMapping("add_force", axis=[1, 0, 0], amplitude=-1),
+            sm.ActionMapping("add_force", axis=[0, 1, 0], amplitude=0.3),
         ],
         n=3,
     )
 
-    r_leg_poly_shifted = shift_polygon(LEG_RIGHT_POLY, lander_init_pos)
-    r_leg = sm.Polygon(points=r_leg_poly_shifted, material=lander_material, parent=lander, name="lander_r_leg")
-    r_leg.mesh.extrude((0, 1, 0), capping=True, inplace=True)
+    r_leg = sm.Polygon(points=LEG_RIGHT_POLY, material=lander_material, parent=lander, name="lander_r_leg")
+    r_leg.mesh.extrude((0, 0, 1), capping=True, inplace=True)
+    r_leg.collider = sm.Collider(type="mesh", convex=True, mesh=r_leg.mesh)
 
-    l_leg_poly_shifted = shift_polygon(LEG_LEFT_POLY, lander_init_pos)
-    l_leg = sm.Polygon(points=l_leg_poly_shifted, material=lander_material, parent=lander, name="lander_l_leg")
-    l_leg.mesh.extrude((0, 1, 0), capping=True, inplace=True)
+    l_leg = sm.Polygon(
+        points=LEG_LEFT_POLY, material=lander_material, parent=lander, name="lander_l_leg"  # l_leg_poly_shifted,
+    )
+    l_leg.mesh.extrude((0, 0, 1), capping=True, inplace=True)
+    l_leg.collider = sm.Collider(type="mesh", convex=True, mesh=l_leg.mesh)
 
     lander.collider = sm.Collider(type="mesh", convex=True, mesh=lander.mesh)
-    # lander.collider = sm.Collider(bounding_box=[2.0, 2.0, 2.0], type="mesh", convex=True, mesh=None)
-
-    # TODO add collider
+    # TODO verify collider
     land = sm.Polygon(
         points=LAND_POLY,
         material=sm.Material.GRAY,
         name="Moon",
     )
-    land.mesh.extrude((0, 1, 0), capping=True, inplace=True)
-    land.collider = sm.Collider(type="mesh", convex=True,  mesh=land.mesh)
-    # land.collider = sm.Collider(bounding_box=[21.0, 1.0, 6.0], type="mesh", convex=True,  mesh=None)
+    land.mesh.extrude((0, 0, 1), capping=True, inplace=True)
+    land.collider = sm.Collider(type="mesh", convex=True, mesh=land.mesh)
+
+    lander += sm.Camera(
+        name="cam",
+        camera_type="orthographic",
+        # TODO Tune position
+        position=np.array([10, 12, 3]) - lander_init_pos,
+        rotation=[0, -180, 0],
+        ymag=10,
+        width=256,
+        height=256,
+    )
 
     sc += lander
     sc += land
@@ -146,136 +155,32 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--build_exe", default=None, type=str, required=False, help="Pre-built unity app for simenv")
     parser.add_argument(
-        "--show",
-        default=False,
-        type=bool,
-        required=False,
-        help="--show=True to show lunarlander in pyvista (and not simulate dynamics)",
-    )
-    parser.add_argument(
         "--num_steps", default=100, type=int, required=False, help="number of steps to run the simulator"
     )
     parser.add_argument("--plot", default=False, type=bool, required=False, help="show camera in matplotlib")
 
     args = parser.parse_args()
 
-    if args.show:
-        sc = make_lander(engine="pyvista")
-        sc.show()  # view_vector=(0, 1, 0)
-        input("press any key to quit")
-    else:
-        sc = make_lander(engine="Unity", engine_exe=args.build_exe)
-        sc += sm.LightSun()
-        sc.lunar_lander += sm.Camera(
-            name="cam",
-            camera_type="orthographic",
-            # TODO Tune position
-            position=[10, 12, 3],
-            rotation=[0, -180, 0],
-            ymag=10,
-            width=512,
-            height=512,
-        )
+    sc = make_lander(engine="Unity", engine_exe=args.build_exe)
+    sc += sm.LightSun()
 
-        # TODO Add agent that acts via thrusters (lateral)
-        sc.show(show_frames=True)
+    sc.show(show_frames=True)
 
-        if args.plot:
-            plt.ion()
-            fig, ax = plt.subplots()
-            imdata = np.zeros(shape=(512, 512, 3), dtype=np.uint8)
-            axim = ax.imshow(imdata, vmin=0, vmax=255)
-            env = sm.RLEnv(sc)
+    if args.plot:
+        plt.ion()
+        fig, ax = plt.subplots()
+        imdata = np.zeros(shape=(256, 256, 3), dtype=np.uint8)
+        axim = ax.imshow(imdata, vmin=0, vmax=255)
+        env = sm.RLEnv(sc)
 
-        for i in range(50):
-            print(f"step {i}")
-            action = 0 #sc.action_space.sample()
-            print(action)
-            # event = env.step([action])
-            obs, reward, done, info = env.step([action])
-            # event = sc.step()
-            # import ipdb; pdb.set_trace()
-
-            # event, _, _, _ = env.step(action=0)
-            if "CameraSensor" in obs:
-                frame = np.array(obs["CameraSensor"], dtype=np.uint8)
-                frame = frame.transpose((1, 2, 0))  # (C,H,W) -> (H,W,C)
-                if args.plot:
-                    axim.set_data(frame[::-1])
-                    fig.canvas.flush_events()
-                    plt.pause(0.01)
-
-#
-# plt.pause(0.5)
-#
-# pdb.set_trace()
-# moon = world.CreateStaticBody(shapes=edgeShape(vertices=[(0, 0), (W, 0)]))
-# sky_polys = []
-# for i in range(CHUNKS - 1):
-#     p1 = (chunk_x[i], smooth_y[i])
-#     p2 = (chunk_x[i + 1], smooth_y[i + 1])
-#     moon.CreateEdgeFixture(vertices=[p1, p2], density=0, friction=0.1)
-#     sky_polys.append([p1, p2, (p2[0], H), (p1[0], H)])
-#
-# moon.color1 = (0.0, 0.0, 0.0)
-# moon.color2 = (0.0, 0.0, 0.0)
-#
-# initial_y = VIEWPORT_H / SCALE
-# lander = world.CreateDynamicBody(
-#     position=(VIEWPORT_W / SCALE / 2, initial_y),
-#     angle=0.0,
-#     fixtures=fixtureDef(
-#         shape=polygonShape(vertices=[(x / SCALE, y / SCALE) for x, y in LANDER_POLY]),
-#         density=5.0,
-#         friction=0.1,
-#         categoryBits=0x0010,
-#         maskBits=0x001,  # collide only with ground
-#         restitution=0.0,
-#     ),  # 0.99 bouncy
-# )
-# lander.color1 = (128, 102, 230)
-# lander.color2 = (77, 77, 128)
-# lander.ApplyForceToCenter(
-#     (
-#         np.random.Generator.uniform(-INITIAL_RANDOM, INITIAL_RANDOM),
-#         np.random.Generator.uniform(-INITIAL_RANDOM, INITIAL_RANDOM),
-#     ),
-#     True,
-# )
-#
-# legs = []
-# for i in [-1, +1]:
-#     leg = world.CreateDynamicBody(
-#         position=(VIEWPORT_W / SCALE / 2 - i * LEG_AWAY / SCALE, initial_y),
-#         angle=(i * LEG_ANGLE),
-#         fixtures=fixtureDef(
-#             shape=polygonShape(box=(LEG_W / SCALE, LEG_H / SCALE)),
-#             density=1.0,
-#             restitution=0.0,
-#             categoryBits=0x0020,
-#             maskBits=0x001,
-#         ),
-#     )
-#     leg.ground_contact = False
-#     leg.color1 = (128, 102, 230)
-#     leg.color2 = (77, 77, 128)
-#     rjd = revoluteJointDef(
-#         bodyA=lander,
-#         bodyB=leg,
-#         localAnchorA=(0, 0),
-#         localAnchorB=(i * LEG_AWAY / SCALE, LEG_DOWN / SCALE),
-#         enableMotor=True,
-#         enableLimit=True,
-#         maxMotorTorque=LEG_SPRING_TORQUE,
-#         motorSpeed=+0.3 * i,  # low enough not to jump back into the sky
-#     )
-#     if i == -1:
-#         rjd.lowerAngle = +0.9 - 0.5  # The most esoteric numbers here, angled legs have freedom to travel within
-#         rjd.upperAngle = +0.9
-#     else:
-#         rjd.lowerAngle = -0.9
-#         rjd.upperAngle = -0.9 + 0.5
-#     leg.joint = world.CreateJoint(rjd)
-#     legs.append(leg)
-#
-# drawlist = [lander] + legs
+    for i in range(500):
+        print(f"step {i}")
+        action = sc.action_space.sample()
+        obs, reward, done, info = env.step([action])
+        if "CameraSensor" in obs:
+            frame = np.array(obs["CameraSensor"], dtype=np.uint8)
+            frame = frame.transpose((1, 2, 0))  # (C,H,W) -> (H,W,C)
+            if args.plot:
+                axim.set_data(frame[::-1])
+                fig.canvas.flush_events()
+                plt.pause(0.001)
