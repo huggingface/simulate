d4a28a15cf8e7d950d74ce59e7a33405e39bc2b4
dylanebert <dylan.ebert@gmail.com>
2022-09-20 14:55:39 -0400

    Unity embedded gltf (#218)
    * added exporting embedded gltf from unity
    
    * added echo gltf test

---
+++ integrations/Unity/simenv-unity/Assets/SimEnv/Runtime/Bridge/TestEchoGLTF.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using SimEnv.GLTF;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace SimEnv {
+    public class TestEchoGLTF : ICommand {
+        public string b64bytes;
+
+        public void Execute(Dictionary<string, object> kwargs, UnityAction<string> callback) {
+            ExecuteAsync(kwargs, callback);
+        }
+
+        async void ExecuteAsync(Dictionary<string, object> kwargs, UnityAction<string> callback) {
+            // Import GLTF
+            byte[] bytes = Convert.FromBase64String(b64bytes);
+            GameObject root = await Importer.LoadFromBytesAsync(bytes);
+
+            // Export GLTF
+            GLTFObject gltfObject = Exporter.CreateGLTFObject(root.transform, null, true);
+            JsonSerializerSettings settings = new JsonSerializerSettings() { NullValueHandling = NullValueHandling.Ignore };
+            string json = JsonConvert.SerializeObject(gltfObject, settings);
+            callback(json);
+        }
+    }
+}
