cdd338eff657f9b5c076c0c3b39c3e4937e2096e
Thomwolf <thomwolf@gmail.com>
2022-09-23 10:45:43 +0200

    updates

--- src/simulate/engine/__init__.py
+++ src/simulate/engine/__init__.py
@@ -2,3 +2,4 @@ from .blender_engine import BlenderEngine
 from .godot_engine import GodotEngine
 from .pyvista_engine import PyVistaEngine
 from .unity_engine import UnityEngine
+from .engine import Engine
