a4ddaec4ee654db6e532da2aa246ce5e8a77751a
dylanebert <dylan.ebert@gmail.com>
2022-09-26 15:41:48 -0400

    fixed simple and mountaincar examples (#243)
    * fixed simple and mountaincar examples
    
    removed extra files
    
    * make style, quality

--- examples/basic/simple_physics.py
+++ examples/basic/simple_physics.py
@@ -34,22 +34,23 @@ def create_scene(build_exe=None):
         bounds=[-10, 10, -0.1, 0, -10, 10],
         material=sm.Material.GRAY75,
         with_collider=True,
+        with_physics_component=False,
     )
 
     # Add a cube that will fall
-    cube = sm.Box(name="cube", position=[0, 3, 0], scaling=[1, 1, 1], material=sm.Material.GRAY50, with_collider=True)
-    scene += cube
-
-    # Add a RigidBodyComponent to the cube
-    # This will enable physics simulation, and cause the cube to fall
-    cube.physics_component = sm.RigidBodyComponent()
+    scene += sm.Box(
+        name="cube",
+        position=[0, 3, 0],
+        scaling=[1, 1, 1],
+        material=sm.Material.GRAY50,
+        with_collider=True,
+        with_physics_component=True,
+    )
 
     # Add a camera to record the scene
     scene += sm.Camera(name="camera", position=[0, 2, -10])
 
     # Calling show() is required for the scene to be initialized
-    # show() allows several engine keyword arguments, which are passed to the Unity backend
-    # You can find all available keyword arguments TODO: here
     scene.show()
 
     return scene
@@ -84,7 +85,7 @@ def simulate(scene, n_frames=30):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--build_exe", help="path to unity engine build executable", required=False, type=str, default=None
+        "--build_exe", help="path to unity engine build executable", required=False, type=str, default=""
     )
     parser.add_argument("-n", "--n_frames", help="number of frames to simulate", required=False, type=int, default=30)
     args = parser.parse_args()
