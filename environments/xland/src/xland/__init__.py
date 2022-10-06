7f06c23e332b40773948c26b41c048d3e11d1631
Nathan Lambert <nol@berkeley.edu>
2022-10-06 10:09:19 -0700

    remove xland

--- environments/xland/src/xland/__init__.py
+++
@@ -1,2 +0,0 @@
-from .gen_map import create_map
-from .rl_scene import make_env_fn
--- environments/xland/src/xland/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-from .constants import *
-from .gen_tiles import generate_tiles
-from .graph import get_connected_components
-from .seed import *
-from .utils import *
