951047e5476fbacf7ffad03908e850793c0fcc87
ClementRomac <clement.romac@gmail.com>
2022-10-28 11:08:29 +0200

    If topology doesn't match requirements, use it as a condition for a new generation instead of regenerating from scratch

--- examples/xland/test_xland.py
+++ examples/xland/test_xland.py
@@ -2,7 +2,7 @@ from env import XLandEnvironment
 import simulate as sm
 
 if __name__ == "__main__":
-    env = XLandEnvironment(9, 9, 6)
+    env = XLandEnvironment(20, 20, 4)
     scene = sm.Scene(engine="Unity")
     scene += sm.LightSun()
     world = env.generate_new_world()
