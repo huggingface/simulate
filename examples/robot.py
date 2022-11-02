7faa5e77b22afa0af4427b622c79ce6ad1038459
Nathan Lambert <nol@berkeley.edu>
2022-11-02 13:43:29 -0700

    tweaking

--- examples/robot.py
+++ examples/robot.py
@@ -29,9 +29,11 @@ def make_scene(build_exe):
     # add light to our scene
     scene += sm.LightSun(name="sun", position=[0, 20, 0], intensity=0.9)
 
-    base = sm.Box(position=[0,0,0], material=sm.Material.GRAY, with_collider=False)
+    base = sm.Box(position=[0, 0, 0], bounds=3.0, material=sm.Material.GRAY, with_collider=False)
     base.physics_component = sm.ArticulationBodyComponent(
-        "fixed", immovable=True, use_gravity=False,
+        "fixed",
+        immovable=True,
+        use_gravity=False,
     )  # note for the base the joint type is ignored
 
     USE_GRAVITY = False
@@ -41,18 +43,21 @@ def make_scene(build_exe):
     link_base = sm.Cylinder(
         name="base_joint",
         is_actor=True,
-        position=[-arm_length / 2.0, arm_length / 2.0, 0],
+        position=[0, arm_length / 2.0, 0],
         radius=arm_radius,
         height=arm_length,
-        rotation=[0, 0, 90.0],
+        rotation=[0, 0, 0]
     )
 
     axis_base = [0.0, 1.0, 0.0]
     link_base.physics_component = sm.ArticulationBodyComponent(
-        "revolute", anchor_position=[-arm_length / 2.0, arm_length / 2.0, 0.0], anchor_rotation=axis_base, use_gravity=USE_GRAVITY,
+        "revolute",
+        anchor_position=[0, 0, 0.0],
+        anchor_rotation=axis_base,
+        use_gravity=USE_GRAVITY,
     )
     mapping = [
-        sm.ActionMapping("add_torque", axis=axis_base, amplitude=1.0),
+        sm.ActionMapping("add_torque", axis=axis_base, amplitude=10.0),
     ]
     link_base.actuator = sm.Actuator(shape=(1,), low=-1.0, high=1.0, mapping=mapping, actuator_tag="base_joint")
 
@@ -60,28 +65,28 @@ def make_scene(build_exe):
     # links = []
     prev_link = link_base
     # for n in reversed(range(num_links, 0, -1)):
-    for n in range(num_links):
-        link = sm.Cylinder(
-            name=f"joint{n}",
-            position=[-arm_length / 2.0, arm_length / 2.0, 0], # x position adjusts for rotation
-            height=arm_length,
-            parent=prev_link,
-            rotation=[0, 0, 90.0],
-            radius=arm_radius,
-        )
-        axis = np.random.rand(3).tolist()
-        link.physics_component = sm.ArticulationBodyComponent(
-            "revolute",
-            anchor_position=[0, -arm_length / 2.0, 0],
-            anchor_rotation=axis,
-            use_gravity=USE_GRAVITY,
-        )
-        mapping = [
-            sm.ActionMapping("add_torque", axis=axis, amplitude=1.0),
-        ]
-        link.actuator = sm.Actuator(shape=(1,), low=-1.0, high=1.0, mapping=mapping, actuator_tag=f"joint{n}")
-
-        prev_link = link
+    # for n in range(num_links):
+    #     link = sm.Cylinder(
+    #         name=f"joint{n}",
+    #         position=[-arm_length / 2.0, arm_length / 2.0, 0],  # x position adjusts for rotation
+    #         height=arm_length,
+    #         parent=prev_link,
+    #         rotation=[0, 0, 90.0],
+    #         radius=arm_radius,
+    #     )
+    #     axis = np.random.rand(3).tolist()
+    #     link.physics_component = sm.ArticulationBodyComponent(
+    #         "revolute",
+    #         anchor_position=[0, -arm_length / 2.0, 0],
+    #         anchor_rotation=axis,
+    #         use_gravity=USE_GRAVITY,
+    #     )
+    #     mapping = [
+    #         sm.ActionMapping("add_torque", axis=axis, amplitude=1.0),
+    #     ]
+    #     link.actuator = sm.Actuator(shape=(1,), low=-1.0, high=1.0, mapping=mapping, actuator_tag=f"joint{n}")
+    #
+    #     prev_link = link
 
     base += link_base
     scene += base
